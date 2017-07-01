adc57b03b24cb55855c8784ee3ceeb91b61b25db
feixia5320 <1044489176@qq.com>
2017-07-01 18:57:26 +0800

    百叶窗特效

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$( document ).click(function() {
-  $( "div" ).effect( "bounce", "slow" );
+$( "#texiao" ).click(function() {
+  $("#texiao2").effect( "bounce", "slow" );
+});
+
+$( "#toggle" ).click(function() {
+  $( "#elem" ).animate({
+    color: "green",
+    backgroundColor: "rgb( 20, 20, 20 )"
+  });
+});
+
+$( "#baiye2" ).click(function() {
+  $( "#baiye" ).toggle( "blind",{direction:"right"} );
 });
